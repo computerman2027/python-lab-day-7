9eadb7cd287126c3e86f38ab910edd4bc4bf8141
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-09-15 12:57:21 +0530

    commit 3

--- p7_1.py
+++ p7_1.py
@@ -1,32 +1,35 @@
 #stack
-stack=[]
-while True:
-    print("MENU\n1. PUSH\n2. POP\n3. PEEK\n4. DISPLAY\n5. EXIT")
-    ch=int(input("Enter your choice : "))
-    if(ch==1):
-        n=int(input("Enter number that need to be pushed : "))
-        stack.append(n)
-    elif(ch==2):
-        if(len(stack)==0):
-            print("UNDEFLOW")
+try:
+    stack=[]
+    while True:
+        print("MENU\n1. PUSH\n2. POP\n3. PEEK\n4. DISPLAY\n5. EXIT")
+        ch=int(input("Enter your choice : "))
+        if(ch==1):
+            n=int(input("Enter number that need to be pushed : "))
+            stack.append(n)
+        elif(ch==2):
+            if(len(stack)==0):
+                print("UNDEFLOW")
+            else:
+                n=stack.pop()
+                print("Popped element =",n)
+        elif(ch==3):
+            if(len(stack)==0):
+                print("Stack is Empty")
+            else:
+                print("Peek =",stack[-1])
+        elif(ch==4):
+            if(len(stack)==0):
+                print("Stack is Empty")
+            else:
+                print("Displaying stack from bottom to top : ",end="")
+                for i in range(len(stack)):
+                    print(stack[i], end=", " if i < len(stack) - 1 else "\n")
+                print()
+        elif(ch==5):
+            print("END OF PROGRAM")
+            break
         else:
-            n=stack.pop()
-            print("Popped element =",n)
-    elif(ch==3):
-        if(len(stack)==0):
-            print("Stack is Empty")
-        else:
-            print("Peek =",stack[-1])
-    elif(ch==4):
-        if(len(stack)==0):
-            print("Stack is Empty")
-        else:
-            print("Displaying stack from bottom to top : ",end="")
-            for i in range(len(stack)):
-                print(stack[i], end=", " if i < len(stack) - 1 else "\n")
-            print()
-    elif(ch==5):
-        print("END OF PROGRAM")
-        break
-    else:
-        print("INVALID INPUT")
+            print("INVALID INPUT")
+except ValueError:
+    print("Error. Only Integer number accepted")
--- p7_2.py
+++ p7_2.py
@@ -1,27 +1,30 @@
 # queue
-queue=[]
-while True:
-    print("MENU\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit")
-    ch=int(input("Enter your choice : "))
-    if ch==1:
-        n=int(input("Enter number that need to be pushed : "))
-        queue.append(n)
-    elif ch==2:
-        if(len(queue)==0):
-            print("UNDEFLOW")
+try:
+    queue=[]
+    while True:
+        print("MENU\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit")
+        ch=int(input("Enter your choice : "))
+        if ch==1:
+            n=int(input("Enter number that need to be pushed : "))
+            queue.append(n)
+        elif ch==2:
+            if(len(queue)==0):
+                print("UNDEFLOW")
+            else:
+                n=queue.pop(0)
+                print("Dequeued element =",n)
+        elif ch==3:
+            if(len(queue)==0):
+                print("Queue is Empty")
+            else:
+                print("Displaying queue from front to rear : ",end="")
+                for i in range(len(queue)):
+                    print(queue[i], end=", " if i < len(queue) - 1 else "\n")
+                print()
+        elif ch==4:
+            print("END OF PROGRAM")
+            break
         else:
-            n=queue.pop(0)
-            print("Dequeued element =",n)
-    elif ch==3:
-        if(len(queue)==0):
-            print("Queue is Empty")
-        else:
-            print("Displaying queue from front to rear : ",end="")
-            for i in range(len(queue)):
-                print(queue[i], end=", " if i < len(queue) - 1 else "\n")
-            print()
-    elif ch==4:
-        print("END OF PROGRAM")
-        break
-    else:
-        print("INVALID INPUT")
+            print("INVALID INPUT")
+except ValueError:
+    print("Error. Only Integer number accepted")
